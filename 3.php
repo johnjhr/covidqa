0cc01e3c8b992d83d5e7fb25ca9ad761a3698f7a
John Jaihar <37814942+johnjaiharjose@users.noreply.github.com>
2020-03-23 21:50:11 +0300

    Add files via upload

---
+++ 3.php
@@ -0,0 +1,49 @@
+<?php
+
+$conn = mysql_connect("YOUR_DB_HOST", "YOUR_DB_NAME", "YOUR_DB_PASS");
+
+if (!$conn) {
+    echo "Unable to connect to DB: " . mysql_error();
+    exit;
+}
+
+if (!mysql_select_db("epiz_24926192_qatarcovid")) {
+    echo "Unable to select mydbname: " . mysql_error();
+    exit;
+}
+
+$sql = "SELECT td, tc, country
+        FROM   data
+        ORDER BY  tc DESC";
+
+$result = mysql_query($sql);
+
+if (!$result) {
+    echo "Could not successfully run query ($sql) from DB: " . mysql_error();
+    exit;
+}
+
+if (mysql_num_rows($result) == 0) {
+    echo "No rows found, nothing to print";
+    exit;
+}
+
+// While a row of data exists, put that row in $row as an associative array
+// Note: If you're expecting just one row, no need to use a loop
+// Note: If you put extract($row); inside the following loop, you'll
+//       then create $userid, $fullname, and $userstatus
+$row = mysql_fetch_assoc($result);
+   // echo $row["id"];
+  //  echo $row["date"];
+  //  echo $row["tc"];
+echo '<span style="font-size: 32pt;color:white">' . $row["td"] . '               '          .                '</span>';
+echo '<img src="ded.png" height="50" width="50"> ';
+
+
+
+mysql_free_result($result);
+
+?>
+
+ 
+
